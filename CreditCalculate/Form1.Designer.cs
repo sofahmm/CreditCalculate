bbc160d71905cd69fa16ae6c7a41a11c3d11a5c8
София Кумышбаева <kimsofocka10@gmail.com>
2021-06-25 11:30:06 +0300

    Создана форма с полями ввода и кнопкой

--- CreditCalculate/Form1.Designer.cs
+++ CreditCalculate/Form1.Designer.cs
@@ -29,39 +29,90 @@
         private void InitializeComponent()
         {
             this.tb_SumInRub = new System.Windows.Forms.TextBox();
-            this.tb_ProcentInDay = new System.Windows.Forms.TextBox();
             this.tb_ZimInDay = new System.Windows.Forms.TextBox();
+            this.lbl_SumInRub = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_PrProcent = new System.Windows.Forms.TextBox();
+            this.btn_rassch = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tb_SumInRub
             // 
-            this.tb_SumInRub.Location = new System.Drawing.Point(135, 100);
+            this.tb_SumInRub.Location = new System.Drawing.Point(221, 76);
+            this.tb_SumInRub.Multiline = true;
             this.tb_SumInRub.Name = "tb_SumInRub";
-            this.tb_SumInRub.Size = new System.Drawing.Size(100, 20);
+            this.tb_SumInRub.Size = new System.Drawing.Size(100, 35);
             this.tb_SumInRub.TabIndex = 0;
             this.tb_SumInRub.TextChanged += new System.EventHandler(this.tb_SumInRub_TextChanged);
             // 
-            // tb_ProcentInDay
-            // 
-            this.tb_ProcentInDay.Location = new System.Drawing.Point(135, 189);
-            this.tb_ProcentInDay.Name = "tb_ProcentInDay";
-            this.tb_ProcentInDay.Size = new System.Drawing.Size(100, 20);
-            this.tb_ProcentInDay.TabIndex = 1;
-            // 
             // tb_ZimInDay
             // 
-            this.tb_ZimInDay.Location = new System.Drawing.Point(135, 144);
+            this.tb_ZimInDay.Location = new System.Drawing.Point(221, 137);
+            this.tb_ZimInDay.Multiline = true;
             this.tb_ZimInDay.Name = "tb_ZimInDay";
-            this.tb_ZimInDay.Size = new System.Drawing.Size(100, 20);
+            this.tb_ZimInDay.Size = new System.Drawing.Size(100, 35);
             this.tb_ZimInDay.TabIndex = 2;
+            this.tb_ZimInDay.TextChanged += new System.EventHandler(this.tb_ZimInDay_TextChanged);
+            // 
+            // lbl_SumInRub
+            // 
+            this.lbl_SumInRub.AutoSize = true;
+            this.lbl_SumInRub.Location = new System.Drawing.Point(44, 98);
+            this.lbl_SumInRub.Name = "lbl_SumInRub";
+            this.lbl_SumInRub.Size = new System.Drawing.Size(122, 13);
+            this.lbl_SumInRub.TabIndex = 3;
+            this.lbl_SumInRub.Text = "Сумма займа в рублях";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Срок займа в днях";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Проценты по дням";
+            // 
+            // tb_PrProcent
+            // 
+            this.tb_PrProcent.Location = new System.Drawing.Point(131, 199);
+            this.tb_PrProcent.Multiline = true;
+            this.tb_PrProcent.Name = "tb_PrProcent";
+            this.tb_PrProcent.Size = new System.Drawing.Size(210, 35);
+            this.tb_PrProcent.TabIndex = 6;
+            this.tb_PrProcent.TextChanged += new System.EventHandler(this.tb_PrProcent_TextChanged);
+            // 
+            // btn_rassch
+            // 
+            this.btn_rassch.Location = new System.Drawing.Point(149, 260);
+            this.btn_rassch.Name = "btn_rassch";
+            this.btn_rassch.Size = new System.Drawing.Size(75, 23);
+            this.btn_rassch.TabIndex = 9;
+            this.btn_rassch.Text = "Рассчитать";
+            this.btn_rassch.UseVisualStyleBackColor = true;
+            this.btn_rassch.Click += new System.EventHandler(this.btn_rassch_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 524);
+            this.ClientSize = new System.Drawing.Size(374, 306);
+            this.Controls.Add(this.btn_rassch);
+            this.Controls.Add(this.tb_PrProcent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_SumInRub);
             this.Controls.Add(this.tb_ZimInDay);
-            this.Controls.Add(this.tb_ProcentInDay);
             this.Controls.Add(this.tb_SumInRub);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -73,8 +124,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox tb_SumInRub;
-        private System.Windows.Forms.TextBox tb_ProcentInDay;
         private System.Windows.Forms.TextBox tb_ZimInDay;
+        private System.Windows.Forms.Label lbl_SumInRub;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_PrProcent;
+        private System.Windows.Forms.Button btn_rassch;
     }
 }
 
--- CreditCalculate/Form1.cs
+++ CreditCalculate/Form1.cs
@@ -21,5 +21,51 @@ namespace CreditCalculate
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_PrDay_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_rassch_Click(object sender, EventArgs e)
+        {
+            var day = tb_ZimInDay.Text;
+            int d = Convert.ToInt32(day);
+            var zim = tb_PrProcent.Text;
+            string[] newZim = zim.Split(';');
+            int procent = 0;
+            for (int i = 0; i <= d; i++)
+            {
+                string a = newZim[i];
+                int ZimN = Convert.ToInt32(a);
+                if (ZimN == 1)
+                {
+                     procent = ZimN * 100;
+                }
+                else
+                {
+                     procent = ZimN * 100 * d;
+                }
+            }
+            MessageBox.Show(Convert.ToString(procent));
+        }
+        private void tb_ZimInDay_TextChanged(object sender, EventArgs e)
+        {
+            var day = tb_ZimInDay.Text;
+        }
+        private void tb_PrProcent_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
